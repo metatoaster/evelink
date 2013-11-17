d2195eedf86c5156af9d78b102927f4e66917444
Tommy Yu <y@metatoaster.com>
2013-11-17 18:24:28 +1300

    Try to mimic more of the real.
    - Have the original test data/xml structure, even though it will take
      more repo space.
    - This will allow the test to be done more closer to the reality level.

--- tests/utils.py
+++ tests/utils.py
@@ -17,6 +17,27 @@ class APITestCase(unittest.TestCase):
     def setUp(self):
         super(APITestCase, self).setUp()
         self.api = mock.MagicMock(spec=evelink_api.API)
+        self.api.format_result = evelink_api.default_result_formatter
+
+    def make_api_result(self, xml_path):
+        return make_api_result(xml_path)
+
+
+class _TestFileAPI(evelink_api.API):
+
+    _result_set = 'raw-1'
+
+    def _get(self, path, params=None):
+        xml_dir = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), 'xml', self._result_set)
+        with open(os.path.join(xml_dir, path + '.xml')) as f:
+            return ElementTree.parse(f)
+
+
+class TestFileAPITestCase(unittest.TestCase):
+    def setUp(self):
+        super(TestFileAPITestCase, self).setUp()
+        self.api = _TestFileAPI()
 
     def make_api_result(self, xml_path):
         return make_api_result(xml_path)
